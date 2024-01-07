2f127854c838d16e8da061c36d95a2096fe5fb69
Suleman Rufai <srufai100@gmail.com>
2024-01-07 21:40:17 +0000

    Betty would be proud

---
+++ 0-betty.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: A simple code that passes the betty check
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	prinf("Alx is the best school!\n");
+
+	return (0);
+
+}
