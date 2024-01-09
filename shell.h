4fc5e26bbad5e63a705b819a139a3f906c078547
Suleman Rufai <srufai100@gmail.com>
2024-01-09 19:41:44 +0000

    Header file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#define MAX_COMMAND_LENGTH 1024
+void _prompt(void);
+void exec_cmd(char *command);
+char *_input(void);
+#endif
+
